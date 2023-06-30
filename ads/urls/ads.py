4e5d01086184bd277de1e6a8f2804636afd4d57d
vvvadim <v.vladimirov@it-park.tech>
2023-06-30 16:34:47 +0300

    fix urls

--- ads/urls/ads.py
+++ ads/urls/ads.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", ad.AdListView.as_view()),
     path("<int:pk>/", ad.AdDetailView.as_view()),
     path("create/", ad.AdCreateView.as_view()),
-    path("<int:pk>/update", ad.AdUpdateView.as_view()),
-    path("<int:pk>/upload_image", ad.AdImageView.as_view()),
-    path("<int:pk>/delete", ad.AdDeleteView.as_view())
+    path("<int:pk>/update/", ad.AdUpdateView.as_view()),
+    path("<int:pk>/upload_image/", ad.AdImageView.as_view()),
+    path("<int:pk>/delete/", ad.AdDeleteView.as_view())
 ]
--- ads/urls/users.py
+++ ads/urls/users.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", user.UserListView.as_view()),
     path("<int:pk>/", user.UserDetailView.as_view()),
     path("create/", user.UserCreateView.as_view()),
-    path("<int:pk>/update", user.UserUpdateView.as_view()),
-    path("<int:pk>/delete", user.UserDeleteView.as_view())
+    path("<int:pk>/update/", user.UserUpdateView.as_view()),
+    path("<int:pk>/delete/", user.UserDeleteView.as_view())
 ]
